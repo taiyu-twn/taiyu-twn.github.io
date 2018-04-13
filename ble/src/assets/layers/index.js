103c7b4b3f4c4314bd07005d1da283074821f01e
Unknown <t104368002@ntut.edu.tw>
2018-04-13 19:05:52 +0800

    Update ble

---
+++ ble/src/assets/layers/index.js
@@ -0,0 +1,12 @@
+import taipei_bike_lane from './taipei_bike_lane.geojson'
+import taipei_exclusive_bike_lane from './taipei_exclusive_bike_lane.geojson'
+import bike_parking_space from './bike_parking_space.geojson'
+import taipei_bike_accidents_geocode from './taipei_bike_accidents_geocode.geojson'
+// import bike_parking_space_heatmap from './bike_parking_space_heatmap.geojson'
+
+export {
+  taipei_bike_lane,
+  taipei_exclusive_bike_lane,
+  bike_parking_space,
+  taipei_bike_accidents_geocode,
+}
---
+++ ble/src/assets/routing/index.js
@@ -0,0 +1,17 @@
+// import demand_small_test from './demand_small_test.json'
+import demand_weekday from './demand_weekday_3.json'
+import demand_weekend from './demand_weekend_3.json'
+// import demand_weekday_3 from './demand_weekday_3.json'
+// import demand_weekday_4 from './demand_weekday_4.json'
+
+export {
+  // demand_small_test,
+  demand_weekday,
+  demand_weekend,
+  // demand_weekday_3,
+  // demand_weekday_4,
+}
+// export { demand_small_test, demand_weekday, demand_weekend }
+// export { demand_small_test, demand_weekday }
+// export { demand_small_test }
+// export { demand_weekday }
